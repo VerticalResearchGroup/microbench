8c3b0b540e67f939b9efbfedf3346702956a7fe0
Tony Nowatzki <tjn@cs.wisc.edu>
2015-04-15 15:07:32 -0500

    Fixed magic instructions

--- common.h
+++ common.h
@@ -1,17 +1,27 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#ifdef MAGIC
+#ifdef FF //Ignore this.
 #include <stdlib.h>
 
-//Magic only works for X86 right now
 #define ROI_BEGIN() \
   __asm__ __volatile__(".byte 0x0f\n\t.byte 0x27\n\t.byte 0x00\n\t.byte 0x0b")
 #define ROI_END() \
   exit(0)
   //_exit(0)
 
-#else // NO MAGIC --doesn't work right now
+#elif MAGIC //current default
+
+//Magic only works for X86 right now
+#define ROI_BEGIN() \
+  __asm__ __volatile__ ("xchg %bx,%bx");
+#define ROI_END() \
+  __asm__ __volatile__ ("xchg %cx,%cx");
+
+
+#define DETAILED_SIM_STOP() \
+    _exit(0)
+#else // Ignore this for now -- put working timing code here
 #define ROI_BEGIN() \
     ticks _Time0, _Time1; \
     _Time0 = _ClockGetTime();
