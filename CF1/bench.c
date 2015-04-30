ee3a4823c56ded83a810ea4e2474f378496ea442
Tony Nowatzki <tjn@cs.wisc.edu>
2015-04-29 22:07:43 -0500

    LFSR Settings Added

--- CF1/bench.c
+++ CF1/bench.c
@@ -32,19 +32,30 @@ int func_no_loopy(int i ){
 __attribute__ ((noinline))
 int loop(int zero) {
   int t = 0,i,iter;
+  ROI_BEGIN();
   for(iter=zero; iter < ITERS; ++iter) {
+    //ROI_BEGIN();
     t+=func_loopy(iter,zero);
+    //ROI_END();
   }
+  ROI_END();
+
+
+  //ROI_BEGIN();
   for(iter=zero; iter < ITERS; ++iter) {
+    //ROI_BEGIN();
     t+=func_no_loopy(iter);
+    //ROI_END();
   }
+  //ROI_END();
+
   return t;
 }
 
 int main(int argc, char* argv[]) {
    argc&=10000;
-   ROI_BEGIN(); 
+   //ROI_BEGIN();
    int t=loop(argc); 
-   ROI_END();
+   //ROI_END();
    volatile int a = t;
 }
